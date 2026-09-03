bb480fef2056c9a4364fe48b266fec0b4b7c3dba
Dominik Kurti <xdominik.kurti@gmail.com>
2026-09-03 17:34:58 +0200

    feat: enhance setup page with user input and progress tracking components

--- src/app/setup/page.tsx
+++ src/app/setup/page.tsx
@@ -7,85 +7,116 @@ import WheelSelector from "@/components/ui/WheelSelector";
 import { languages } from "@/values/languages";
 import ProgressBar from "@/components/ui/ProgressBar";
 import Input from "@/components/ui/Input";
+import { LuUser } from "react-icons/lu";
+import { MdOutlineCameraAlt } from "react-icons/md";
+import { PiPassword } from "react-icons/pi";
+import { IoFingerPrint } from "react-icons/io5";
 
 const SetupPage = () => {
-    const steps = ["Welcome", "Language", "Profile", "Password", "Biometrics", "Finish"];
+  const steps = ["Welcome", "Language", "Profile", "Password", "Biometrics", "Finish"];
 
-    const [swiper, setSwiper] = useState<SwiperClass | null>(null);
-    const [activePage, setActivePage] = useState(0);
+  const [swiper, setSwiper] = useState<SwiperClass | null>(null);
+  const [activePage, setActivePage] = useState(0);
 
-    return (
-        <>
-            <Swiper
-                className="w-full h-full"
-                slidesPerView={1}
-                spaceBetween={50}
-                onSwiper={(swiper) => {
-                    setSwiper(swiper);
-                }}
-                onSlideChange={(swiper) => {
-                    setActivePage(swiper.activeIndex);
-                }}
-            >
-                <SwiperSlide className="flex! flex-col! items-center! justify-start gap-6">
-                    <header className="w-full min-h-fit flex flex-col items-center justify-start p-4 gap-4">
-                        <h3 className="font-semibold text-center">Choose your language</h3>
-                        <p className="text-center">We've set this based on your device — change it if needed.</p>
-                    </header>
-                    <div className="w-full h-full flex items-center justify-center p-4">
-                        <WheelSelector options={languages} visible={25} />
-                    </div>
-                </SwiperSlide>
+  const [biometrics, setBiometrics] = useState<boolean>(false);
 
-                <SwiperSlide className="flex! flex-col! items-center! justify-start gap-4">
-                    <header className="w-full min-h-fit flex flex-col items-center justify-start p-4 gap-4">
-                        <h3 className="font-semibold text-center">What should we call you?</h3>
-                        <p className="text-center">This just personalizes your vault — it's stored locally and never leaves your device.</p>
-                    </header>
-                    <div className="w-fit h-full flex flex-col items-center justify-center gap-4">
-                        <div className="w-60 h-60 bg-amber-300 rounded-2xl"></div>
-                        <Input />
-                    </div>
-                </SwiperSlide>
-                
-                <SwiperSlide className="flex! flex-col! items-center! justify-start gap-4">
-                    <header className="w-full min-h-fit flex flex-col items-center justify-start p-4 gap-4">
-                        <h3 className="font-semibold text-center">Create your master password</h3>
-                        <p className="text-center">This is the only password you'll ever need to remember. It encrypts everything in your vault.</p>
-                    </header>
-                </SwiperSlide>
-                
-                <SwiperSlide className="flex! flex-col! items-center! justify-start gap-4">
-                    <header className="w-full min-h-fit flex flex-col items-center justify-start p-4 gap-4">
-                        <h3 className="font-semibold text-center">Unlock in a glance</h3>
-                        <p className="text-center">Use Face ID or your fingerprint to get into your vault instantly.</p>
-                    </header>
-                </SwiperSlide>
-                
-                <SwiperSlide className="flex! flex-col! items-center! justify-start gap-4">
-                    <header className="w-full min-h-fit flex flex-col items-center justify-start p-4 gap-4">
-                        <h3 className="font-semibold text-center">You're all set, [Name]</h3>
-                        <p className="text-center">Your vault is ready.</p>
-                    </header>
-                </SwiperSlide>
-            </Swiper>
-            <ProgressBar options={steps} percentage={(100 / (steps.length)) * ( 2 + activePage)} />
-            <footer className="w-full h-fit flex flex-row  items-center justify-center gap-4">
-                <button className="bg-transparent w-2/5 h-fit flex items-center justify-center py-4 px-8 rounded-full" onClick={() => { swiper?.slidePrev() }}>
-                    <p className="font-semibold text-(--text)">Back</p>
-                </button>
-                <button className="bg-(--accent) w-full h-fit flex items-center justify-center p-4 rounded-full" onClick={() => { swiper?.slideNext() }}>
-                    <p className="font-semibold text-(--text)">{swiper && activePage == (swiper?.slides.length - 1) ? "Get started" : "Confirm"}</p>
-                </button>
-                {/*<button
-                    className="w-full h-fit flex items-center justify-center p-4 rounded-full"
-                    style={{ opacity: swiper && activePage == 0 ? "0.25" : "1" }}
-                    onClick={() => { swiper?.slidePrev() }}>
-                    <p className="font-semibold text-(--text)">Back</p>
-                </button>*/}
-            </footer>
-        </>
-    );
+  return (
+    <>
+      <Swiper
+        className="w-full h-full"
+        slidesPerView={1}
+        spaceBetween={50}
+        onSwiper={(swiper) => {
+          setSwiper(swiper);
+        }}
+        onSlideChange={(swiper) => {
+          setActivePage(swiper.activeIndex);
+        }}
+      >
+        <SwiperSlide className="flex! flex-col! items-center! justify-start gap-6">
+          <header className="w-full min-h-fit flex flex-col items-center justify-start p-4 gap-4">
+            <h3 className="font-semibold text-center">Choose your language</h3>
+            <p className="text-center">We've set this based on your device — change it if needed.</p>
+          </header>
+          <div className="w-full h-full flex items-center justify-center p-4">
+            <WheelSelector options={languages} visible={25} />
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide className="flex! flex-col! items-center! justify-start gap-4">
+          <header className="w-full min-h-fit flex flex-col items-center justify-start p-4 gap-4">
+            <h3 className="font-semibold text-center">What should we call you?</h3>
+            <p className="text-center">This just personalizes your vault — it's stored locally and never leaves your device.</p>
+          </header>
+          <div className="w-full h-full flex flex-col items-center justify-center gap-8">
+            <div className="relative w-40 h-40 flex items-center justify-center border-2 border-dashed rounded-full bg-(--text)/5">
+              <LuUser className="w-auto h-3/10" />
+              <button className="absolute bg-(--accent) bottom-0 right-0 w-12 h-12 border-6 border-(--background) flex flex-col items-center justify-center rounded-full">
+                <MdOutlineCameraAlt className="w-auto h-6" />
+              </button>
+            </div>
+            <Input className="w-9/10 h-fit bg-(--background) shadow rounded-xl p-3" placeholder="Hi, my name is..." />
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide className="flex! flex-col! items-center! justify-start gap-4">
+          <header className="w-full min-h-fit flex flex-col items-center justify-start p-4 gap-4">
+            <h3 className="font-semibold text-center">Create your master password</h3>
+            <p className="text-center">This is the only password you'll ever need to remember. It encrypts everything in your vault.</p>
+          </header>
+          <div className="w-full h-full flex flex-col items-center justify-center gap-8">
+            <Input className="w-9/10 h-fit bg-(--background) shadow rounded-xl p-3" placeholder="Password" Icon={PiPassword} type="password" />
+            <Input className="w-9/10 h-fit bg-(--background) shadow rounded-xl p-3" placeholder="Password Again" Icon={PiPassword} type="password" />
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide className="flex! flex-col! items-center! justify-start gap-4">
+          <header className="w-full min-h-fit flex flex-col items-center justify-start p-4 gap-4">
+            <h3 className="font-semibold text-center">Unlock in a glance</h3>
+            <p className="text-center">Use Face ID or your fingerprint to get into your vault instantly.</p>
+          </header>
+          <div className="w-full h-full flex flex-col items-center justify-center gap-8">
+            <button className="h-2/3 w-fit" onClick={() => setBiometrics(!biometrics)}>
+              <IoFingerPrint className="w-auto h-full duration-300" style={{ opacity: biometrics ? "1" : "0.25", color: biometrics ? "var(--accent)" : "" }} />
+            </button>
+            <p className="duration-300" style={{ opacity: biometrics ? "1" : "0.5" }}>
+              Biometrics has been{" "}
+              <span className="font-semibold" style={{ opacity: biometrics ? "1" : "0.5", color: biometrics ? "var(--accent)" : "" }}>
+                {biometrics ? "Enabled" : "Disabled"}
+              </span>
+              .
+            </p>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide className="flex! flex-col! items-center! justify-start gap-4">
+          <header className="w-full min-h-fit flex flex-col items-center justify-start p-4 gap-4">
+            <h3 className="font-semibold text-center">You're all set, [Name]</h3>
+            <p className="text-center">Your vault is ready.</p>
+          </header>
+        </SwiperSlide>
+      </Swiper>
+      <ProgressBar options={steps} percentage={(100 / steps.length) * (2 + activePage)} />
+      <footer className="w-full h-fit flex flex-row  items-center justify-center gap-4">
+        <button
+          className="bg-transparent w-2/5 h-fit flex items-center justify-center py-4 px-8 rounded-full"
+          onClick={() => {
+            swiper?.slidePrev();
+          }}
+        >
+          <p className="font-semibold text-(--text)">Back</p>
+        </button>
+        <button
+          className="bg-(--accent) w-full h-fit flex items-center justify-center p-4 rounded-full"
+          onClick={() => {
+            swiper?.slideNext();
+          }}
+        >
+          <p className="font-semibold text-(--text)">{swiper && activePage == swiper?.slides.length - 1 ? "Get started" : "Confirm"}</p>
+        </button>
+      </footer>
+    </>
+  );
 };
 
 export default SetupPage;
--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,11 +1,34 @@
-const Input = () => {
-    return <div className="">
-        <div>
-            <input className="" placeholder="Mizu" />
-            <p></p>
+import { useState } from "react";
+import { IconType } from "react-icons";
+import { LuEye, LuEyeClosed } from "react-icons/lu";
+
+type ElementProps = {
+  className?: string;
+  placeholder?: string;
+  Icon?: IconType;
+  type?: string;
+};
+
+const Input = (props: ElementProps) => {
+  const { className, placeholder, Icon, type } = props;
+  const [show, setShow] = useState<boolean>(false);
+
+  return (
+    <div className={className}>
+      <div className="w-full h-fit flex flex-row items-center justify-center gap-2">
+        {Icon && <Icon className="h-6 w-auto" />}
+        <div className="relative w-full h-fit duration-200 has-[input:not(:placeholder-shown)]:translate-y-3/10">
+          <input className="peer w-full h-fit outline-none" placeholder="" type={show ? "string" : type} />
+          <p className="absolute left-0 top-1/2 origin-left -translate-y-1/2 peer-not-placeholder-shown:-top-1/4 peer-not-placeholder-shown:scale-75 duration-200 peer-not-placeholder-shown:opacity-50 pointer-events-none">{placeholder}</p>
         </div>
-        <div></div>
+        {type == "password" && (
+          <button className="bg-(--accent) min-h-8 min-w-8 flex items-center justify-center rounded-lg shadow" onClick={() => setShow(!show)}>
+            {show ? <LuEye className="w-auto h-5" /> : <LuEyeClosed className="w-auto h-5" />}
+          </button>
+        )}
+      </div>
     </div>
-}
+  );
+};
 
 export default Input;
